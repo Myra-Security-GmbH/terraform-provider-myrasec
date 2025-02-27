d0b8f9def8e696d78e40a585cfaa9e20f6ff7e26
Tim Spannan <tim.spannan@myrasecurity.com>
2025-02-27 17:01:54 +0100

    also use content hash for maintenance template

--- myrasec/resource_myrasec_maintenance_template.go
+++ myrasec/resource_myrasec_maintenance_template.go
@@ -63,6 +63,15 @@ func resourceMyrasecMaintenanceTemplate() *schema.Resource {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Description: "HTML content of the maintenance template.",
+				DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
+					oldHash := d.Get("content_hash")
+					newHash := createContentHash(newValue)
+					return oldHash == newHash
+				},
+			},
+			"content_hash": {
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 		},
 		Timeouts: &schema.ResourceTimeout{
@@ -266,7 +275,8 @@ func resourceMyrasecMaintenanceTemplateImport(ctx context.Context, d *schema.Res
 	d.Set("domain_name", domainName)
 	d.Set("maintenance_id", template.ID)
 	d.Set("name", template.Name)
-	d.Set("content", template.Content)
+	d.Set("content", "")
+	d.Set("content_hash", createContentHash(template.Content))
 
 	resourceMyrasecMaintenanceTemplateRead(ctx, d, meta)
 
@@ -356,6 +366,7 @@ func setMaintenanceTemplateData(d *schema.ResourceData, template *myrasec.Mainte
 	d.Set("created", template.Created.Format(time.RFC3339))
 	d.Set("modified", template.Modified.Format(time.RFC3339))
 	d.Set("name", template.Name)
-	d.Set("content", template.Content)
+	d.Set("content", "")
+	d.Set("content_hash", createContentHash(template.Content))
 	d.Set("domain_id", domainID)
 }
